6418b46221457ba7dba87f4454aaea5383e12f1d
ToastFish <2270499893@qq.com>
2024-11-04 23:24:47 +0800

    管理用户前端页面初步完成，后端分页查询接口完成

---
+++ web/src/views/main/admin-user.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>这是管理员用户界面</p>
+    </div>
+</template>
+<script>
+import { defineComponent} from 'vue';
+export default defineComponent({
+    name:'admin-user',
+    setup(){}
+})
+</script>
+<style>
+    
+</style>
